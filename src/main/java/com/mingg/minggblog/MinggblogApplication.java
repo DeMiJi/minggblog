d22e51d8daf7d234ac9cfe298fc043b383dac157
Ashen <2890340612@qq.com>
2021-07-25 01:00:23 +0800

    Initial commit

---
+++ src/main/java/com/mingg/minggblog/MinggblogApplication.java
@@ -0,0 +1,13 @@
+package com.mingg.minggblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinggblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinggblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mingg/minggblog/MinggblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingg.minggblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinggblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
