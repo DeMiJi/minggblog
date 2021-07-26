6541d5f7d0027dc04716ef806582a0e4df4c398d
Ashen <2890340612@qq.com>
2021-07-27 00:50:05 +0800

    日志

--- src/main/java/com/mingg/minggblog/web/indexController.java
+++ src/main/java/com/mingg/minggblog/web/indexController.java
@@ -3,16 +3,18 @@ package com.mingg.minggblog.web;
 import com.mingg.minggblog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class indexController {
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name){
 //        int i =9/0;
-        String blog = null;
-        if (blog == null){
-            throw new NotFoundException("博客找不到");
-        }
+//        String blog = null;
+//        if (blog == null){
+//            throw new NotFoundException("博客找不到");
+//        }
+        System.out.println("-------index-------");
         return "index";
     }
 }
